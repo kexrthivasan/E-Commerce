5a5dbcca91846c82f9c8a22df4d5e7b3df38d0ff
kexrthivasan <keerthivasangs2004@gmail.com>
2024-07-02 20:09:13 +0530

    deploy

--- Ecommerce/wsgi.py
+++ Ecommerce/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
+settings_module = 'Ecommerce.deployment' if 'WEBSITE_hOSTNAME' os.environ else 'Ecommerce.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
 
 application = get_wsgi_application()
