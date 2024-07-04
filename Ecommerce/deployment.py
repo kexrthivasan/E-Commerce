63554be9bb625a5253bca2256c674df131aa5f5f
kexrthivasan <keerthivasangs2004@gmail.com>
2024-07-04 13:22:19 +0530

    final

--- Ecommerce/deployment.py
+++ Ecommerce/deployment.py
@@ -1,14 +1,14 @@
 import os
-from . settings import
-from . settings import BASE_DIR
+from .settings import BASE_DIR
+
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS=[os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS=['https://'+ os.environ['WEBSITE_HOSTNAME']]
-DEBUG = false
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddle'
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Fixed middleware name
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -17,18 +17,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters=(pair.split('='):pair.split('=')[1] for pair in connection_string.split(' '))
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': parameters['dbname'],
-        'HOST':parameters['host'],
-        'USER':parameters['user'],
-        'PASSWORD':parameters['password'],
-        
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
     }
 }
